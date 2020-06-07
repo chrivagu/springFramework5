43ecc7b40e23bcb308a817355b6be2421a1132ff
Christian Valencia Gutierrez <Christian.X.Valencia.-ND@disney.com>
2020-06-07 11:01:10 -0500

    Inyectar valores usando la anotacion @Value

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.web.app.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,39 +16,46 @@ import com.bolsadeideas.springboot.web.app.models.Usuario;
 @RequestMapping("/app")
 public class IndexController {
 
-	@GetMapping({"/index", "/", "/home"})
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textoListar;
+
+	@GetMapping({ "/index", "/", "/home" })
 	public String index(Model model) {
-		model.addAttribute("titulo", "Hola Spring Framework con Model!");
+		model.addAttribute("titulo", textoIndex);
 		return "index";
 	}
-	
+
 	@RequestMapping("/perfil")
 	public String perfil(Model model) {
-		
+
 		Usuario usuario = new Usuario();
 		usuario.setNombre("Andrés");
 		usuario.setApellido("Guzmán");
 		usuario.setEmail("andres@correo.com");
-		
+
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil de usuario: ".concat(usuario.getNombre()));
+		model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
 		return "perfil";
 	}
-	
-	
+
 	@RequestMapping("/listar")
-	public String listar(Model model) {		
-		model.addAttribute("titulo", "Listado de usuarios");
+	public String listar(Model model) {
+		model.addAttribute("titulo", textoListar);
 		return "listar";
 	}
-	
+
 	@ModelAttribute("usuarios")
 	public List<Usuario> poblarUsuarios() {
 		List<Usuario> usuarios = Arrays.asList(new Usuario("Andrés", "Guzmán", "andres@correo.com"),
-				new Usuario("John", "Doe", "andres@correo.com"),
-				new Usuario("Jane", "Doe", "jane@correo.com"),
+				new Usuario("John", "Doe", "andres@correo.com"), new Usuario("Jane", "Doe", "jane@correo.com"),
 				new Usuario("Tornado", "Roe", "roe@correo.com"));
-		
+
 		return usuarios;
 	}
 
