1eb987d9d714cc40cb2d29d633a9c4ff21ab5104
Christian Valencia Gutierrez <Christian.X.Valencia.-ND@disney.com>
2020-07-03 21:39:35 -0500

    Anotacion @Primary

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -1,12 +1,14 @@
 package com.bolsadeideas.springboot.di.app.models.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component("miServicioSimple")
 public class MiServicio implements IServicio {
 
 	public String operacion() {
-		return "ejecutando algún proceso importante!..";
+		return "ejecutando algún proceso importante simple!..";
 	}
 
 }
