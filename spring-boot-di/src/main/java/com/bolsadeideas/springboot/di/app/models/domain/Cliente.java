4c1af74cb297137eda3d8695e78136ea9c3bc7cd
Christian Valencia Gutierrez <Christian.X.Valencia.-ND@disney.com>
2020-07-04 17:23:54 -0500

    Ejemplo factura y relaciones entre sus componentes

---
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Cliente.java
@@ -0,0 +1,30 @@
+package com.bolsadeideas.springboot.di.app.models.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cliente {
+	
+	@Value("${cliente.nombre}")
+	private String nombre;
+	
+	@Value("${cliente.apellido}")
+	private String appellido;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getAppellido() {
+		return appellido;
+	}
+	public void setAppellido(String appellido) {
+		this.appellido = appellido;
+	}
+	
+	
+
+}
