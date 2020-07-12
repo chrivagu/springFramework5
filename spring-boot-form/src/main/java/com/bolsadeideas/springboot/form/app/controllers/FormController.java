2f8b58837b72184f1510ab4e9ed410d021cee9a9
Christian Valencia Gutierrez <Christian.X.Valencia.-ND@disney.com>
2020-07-12 16:47:56 -0500

    El atributo object de thymeleaf en el elemento form

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,8 +1,5 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -29,12 +26,12 @@ public class FormController {
 		model.addAttribute("titulo", "Resultado form");
 
 		if (result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
+			/*Map<String, String> errores = new HashMap<>();
 			result.getFieldErrors().forEach(err -> {
 				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ")
 						.concat(err.getDefaultMessage()));
 			});
-			model.addAttribute("error", errores);
+			model.addAttribute("error", errores);*/
 			return "form";
 		}
 		
