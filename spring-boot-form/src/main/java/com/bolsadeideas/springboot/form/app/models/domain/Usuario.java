729f78aef654a9cbd75f92dace6b4e7ce3c257eb
Christian Valencia Gutierrez <Christian.X.Valencia.-ND@disney.com>
2020-07-15 10:34:28 -0500

    Validación de números enteros con @Max @Min y NotNull

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,8 +1,11 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 //import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -32,6 +35,11 @@ public class Usuario {
 	@Requerido
 	@Email(message = "correo con formato incorrecto")
 	private String email;
+	
+	@NotNull
+	@Min(5)
+	@Max(5000)
+	private Integer cuenta;
 
 	public String getUsername() {
 		return username;
@@ -80,5 +88,13 @@ public class Usuario {
 	public void setIdentificador(String identificador) {
 		this.identificador = identificador;
 	}
+
+	public Integer getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(Integer cuenta) {
+		this.cuenta = cuenta;
+	}		
 
 }
