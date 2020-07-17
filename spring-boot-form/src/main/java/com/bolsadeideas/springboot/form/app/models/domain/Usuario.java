041a2baff8a52ffbf76ffeb09f3ce36fe25fc78a
Christian Valencia Gutierrez <Christian.X.Valencia.-ND@disney.com>
2020-07-16 21:28:09 -0500

    Lista select desplegable

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -50,6 +50,9 @@ public class Usuario {
 	@Past
 	//@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date fechaNacimiento;
+	
+	@NotEmpty
+	private String pais;
 
 	public String getUsername() {
 		return username;
@@ -113,6 +116,14 @@ public class Usuario {
 
 	public void setFechaNacimiento(Date fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
-	}	
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
 
 }
